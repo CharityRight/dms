9e7294d7baf5edfb0159e489ef62faa2e479d2a6
Hamza Khan-Cheema <hamzakc@gmail.com>
2018-02-17 15:46:39 +0300

    [#16]: Add API endpoint to retrieve single cause by code

---
+++ apps/main/lib/dms/main/causes/operations/get.rb
@@ -0,0 +1,26 @@
+require 'dms/main/import'
+require 'dms/main/entities/cause'
+require 'dms/main/causes/validations/cause'
+require 'dms/matcher'
+
+module Dms
+  module Main
+    module Causes
+      module Operations
+        class Get
+          include Dms::Matcher
+          include Dms::Main::Import["cause_repo"]
+
+          def call(code)
+            cause = cause_repo.get_by_code(code)
+            if cause
+              Dry::Monads::Right(cause)
+            else
+              Dry::Monads::Left(errors: "Cause #{code} not found")
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- apps/main/web/routes/cause.rb
+++ apps/main/web/routes/cause.rb
@@ -1,9 +1,9 @@
 module Dms
   module Main
     class Web
-      route "causes" do |r|
+      route 'causes' do |r|
         r.post do
-          r.resolve "causes.operations.create" do |create|
+          r.resolve 'causes.operations.create' do |create|
             create.(r["data"]) do |m|
               m.success do |created_cause|
                 response.status = 201
@@ -16,10 +16,29 @@ module Dms
             end
           end
         end
-        r.get do
-          r.resolve "causes.operations.index" do |index|
-            results = index.call
-            results.mapper.model.collection_to_json_api(results.to_a)
+        r.on do
+          r.on :code do |code|
+            r.get do
+              r.resolve 'causes.operations.get' do |cause|
+                cause.(code) do |m|
+                  m.success do |cause_found|
+                    cause_found.to_json_api
+                  end
+                  m.failure do |errors|
+                    response.status = 404 # Malformed request
+                    errors.to_json
+                  end
+                end
+              end
+            end
+          end
+          r.is do
+            r.get do
+              r.resolve 'causes.operations.index' do |index|
+                results = index.call
+                results.mapper.model.collection_to_json_api(results.to_a)
+              end
+            end
           end
         end
       end
